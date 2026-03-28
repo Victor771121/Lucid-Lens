677a2055705d1b4233eb306c86d0daf0ca099edd
chisom0x <onyenankiekelvin@gmail.com>
2026-03-28 16:15:28 +0100

    feat: install and configure cors middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import aiRoutes from './route/ai.route.js';
+import cors from 'cors';
 dotenv.config();
 
 export const createServer = () => {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
 
